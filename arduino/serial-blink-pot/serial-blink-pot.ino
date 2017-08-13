ffd53f8244e91a959d5cdcbb588c7ff5b2752ff1
Stephanie Smith <stephiesmith@gmail.com>
2017-08-13 15:54:53 -0700

    revising diagrams to match tests and updating arduino code

--- arduino/serial-blink-pot/serial-blink-pot.ino
+++ arduino/serial-blink-pot/serial-blink-pot.ino
@@ -17,12 +17,17 @@
   - use two switches to nest mode (will this require metro?)
 */
 
-int knobPin = A0;
-int ledPin = LED_BUILTIN; // 13
-int knobValue = 0; // variable for reading the knob
 
-int switchPin = 4;
-int switchState = 0; // variable for reading the switch
+int ledPin = LED_BUILTIN; // 13
+
+int knobPin = A0;     // analog pin because range of numbers
+int knobValue = 0;    // variable for reading the knob
+
+int switchPin = 7;    // digital pin because binary (on/off)
+int switchState = 0;  // variable for reading the switch
+
+int buttonPin = 2;    // digital pin because binary  (on/off)
+int buttonState = 0;  // variable for reading the button
 
 void setup() {
   
@@ -31,8 +36,11 @@ void setup() {
   // initialize digital pin LED_BUILTIN as an output.
   pinMode(ledPin, OUTPUT);
   
-  // initialize the pushbutton pin as an input:
+  // initialize the switch pin as an input:
   pinMode(switchPin, INPUT);
+
+  // initialize the button pin as an input:
+  pinMode(buttonPin, INPUT);
 
   // start serial communications at 9600 baud
   Serial.begin(9600);
@@ -40,37 +48,47 @@ void setup() {
 }
 
 void loop() {
+  
   // must read it repeatedly to repeatedly evaluate this conditional
+  buttonState = digitalRead(buttonPin);
+  Serial.println(buttonState);
 
-  switchState = digitalRead(switchPin);
-  Serial.println(switchState);
-  knobValue = analogRead(knobPin);
-
-  if (switchState == 1) {
-    if (knobValue < 200) {
-      // read the value from the knob:
-      Serial.println(knobValue);
-      // turn the ledPin on
-      digitalWrite(ledPin, HIGH);
-      // stop the program for <knobValue> milliseconds:
-      delay(knobValue);
-      // turn the ledPin off:
-      digitalWrite(ledPin, LOW);
-      // stop the program for for <knobValue> milliseconds:
-      delay(knobValue);    
-    } else {
-      digitalWrite(ledPin, LOW);
-      Serial.print("Off, ");
-      Serial.println(knobValue);
-      delay(knobValue);
-    }    
+  if (buttonState == HIGH) {
+    
+    // just turn it on no matter what
+    digitalWrite(ledPin, HIGH);
+    
   } else {
-    // off
+    
+    // do the complicated stuff
+    switchState = digitalRead(switchPin);
     Serial.println(switchState);
-    delay(500);
-  }
-
-
-
+    knobValue = analogRead(knobPin);
+  
+    if (switchState == 1) {
+      if (knobValue < 200) {
+        // read the value from the knob:
+        Serial.println(knobValue);
+        // turn the ledPin on
+        digitalWrite(ledPin, HIGH);
+        // stop the program for <knobValue> milliseconds:
+        delay(knobValue);
+        // turn the ledPin off:
+        digitalWrite(ledPin, LOW);
+        // stop the program for for <knobValue> milliseconds:
+        delay(knobValue);    
+      } else {
+        digitalWrite(ledPin, LOW);
+        Serial.print("Off, ");
+        Serial.println(knobValue);
+        delay(knobValue);
+      }    
+    } else {
+      // off
+      Serial.println(switchState);
+      delay(500);
+    } // end of switchState logic
+    
+  } // end of buttonState logic
 
 }
