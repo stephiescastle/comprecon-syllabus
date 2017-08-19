7dfae5a2b559189d1773fd664597d99dca10e413
Stephanie Smith <stephiesmith@gmail.com>
2017-08-19 16:25:17 -0700

    cleaning up servo demo

--- arduino/Extras/servo-control/servo-control.ino
+++ arduino/Extras/servo-control/servo-control.ino
@@ -5,16 +5,16 @@
 
  Servos can only move 0 - 180
 
-  Misbehaving servo?
-  https://learn.adafruit.com/adafruit-arduino-lesson-14-servo-motors/if-the-servo-misbehaves
+ Misbehaving servo?
+ https://learn.adafruit.com/adafruit-arduino-lesson-14-servo-motors/if-the-servo-misbehaves
   
 **************************************/
 #include <Servo.h>
 
 Servo myservo;
 
-int sensorPin = A0;     // analog pin because reading a voltage (range of numbers)
-int sensorValue = 0;    // variable for reading the sensor
+int knobPin = A0;     // analog pin because reading a voltage (range of numbers)
+int knobValue = 0;    // variable for reading the sensor
 
 void setup() {
   
@@ -29,12 +29,12 @@ void setup() {
 void loop() {
   // main program goes here
   
-  sensorValue = analogRead(sensorPin);
-  Serial.print(sensorValue);      // print out the value
-  sensorValue = map(sensorValue, 0, 600, 0, 180);
+  knobValue = analogRead(knobPin);
+  Serial.print(knobValue);
+  knobValue = map(knobValue, 0, 600, 0, 180); // maps values to range of servo
   Serial.print(", Mapped to: ");
-  Serial.println(sensorValue);
-  myservo.write(sensorValue); 
-  delay(100);
+  Serial.println(knobValue);
+  myservo.write(knobValue); 
+  delay(15);
 
 }
